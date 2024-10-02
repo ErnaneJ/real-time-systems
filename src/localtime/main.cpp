6d233babaade1be7c9b9cf9ba5860e9e2e1125dd
Ernane Ferreira <ernane.junior25@gmail.com>
2024-10-01 22:11:17 -0300

    feat: add localtime code

---
+++ src/localtime/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() 
+{
+	struct tm *timePointer;  
+	time_t currentTime;
+
+	time(&currentTime);
+	timePointer = localtime(&currentTime);
+
+	cout << "Current timestamp: " 
+			 << timePointer->tm_mday << "/" 
+			 << timePointer->tm_mon + 1 << "/" 
+			 << timePointer->tm_year + 1900 << "\t" 
+			 << timePointer->tm_hour << ":" 
+			 << timePointer->tm_min << ":" 
+			 << timePointer->tm_sec << "\n";
+
+	return 0;
+};
